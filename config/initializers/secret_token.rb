54fa8588caf2e8a81533e875a5359e7c3e0b9c8a
Sugino Yasuhiro <suginoyasuhiro@gmail.com>
2013-08-03 22:11:22 +0900

    New Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebDesignSchool::Application.config.secret_key_base = 'ffb22e883ca13a261a60c63db0f0ab0d94a577752ab9ef947bbbc61000ea9678d2b8bad2fefb79a08a931c40101a7d275f7eba38d3eba711e48ebaf5dad8ba29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebDesignSchool::Application.config.session_store :cookie_store, key: '_WebDesignSchool_session'
